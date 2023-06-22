1bcea9bda115422d856d2ce17e85ef725020ec8c
aeramisant <aeramisant@griddynamics.com>
2023-06-22 18:07:52 +0300

    add server-side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
+import path from 'path';
+import fs from 'fs/promises';
+interface Product {
+  id: string;
+  title: string;
+  desription: string;
+}
+const Home = (props: { products: Product[] }) => {
+  const { products } = props;
   return (
     <>
       <h1>This is the home page of the app</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        <li>Product 4</li>
-        <li>Product 5</li>
+        {products.map((product: Product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </>
   );
+};
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  console.log(data);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
+export default Home;
